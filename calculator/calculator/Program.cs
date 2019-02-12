650eefeb689d7a21933b5b693d92dc57bafeb2b1
yolanda <ysimpson@spartaglobal.com>
2019-02-12 10:52:54 +0000

    calc

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+
+            int num2 = 0;
+
+            float answer = 0.0f;
+
+
+
+
+
+            Console.Write("Please enter the first number: ");
+            //convert given input to integer 
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            // to add numbers together 
+            Console.WriteLine("+");
+        
+            Console.Write("Please enter the second number: ");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            {
+                answer = num1 + num2;
+                Console.WriteLine(num1 + " + " + num2 + " = " + answer);
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
